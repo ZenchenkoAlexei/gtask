08bd5d5249639873cfe9d2310bd3e0b36de68447
Alexey <zz88@tut.by>
2017-11-04 17:28:30 +0300

    vrode rabotaet

--- travel/models.py
+++ travel/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    username = models.CharField(max_length=50)
+    # id = models.AutoField(primary_key=True, unique=True)
+    username = models.CharField(primary_key=True, max_length=50)
     email = models.EmailField(blank=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -15,17 +15,17 @@ class User(models.Model):
 
 
 class Location(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
+    # id = models.AutoField(primary_key=True, unique=True)
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
-    name = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(primary_key=True, max_length=50)
     description = models.TextField(max_length=250)
 
 
 class Visit(models.Model):
     CHOICES = [(i, i) for i in range(11)]
     # LOCATIONS = [(i, i) for i in Location.objects.values_list()]
-    id = models.AutoField(primary_key=True, unique=True)
+    # id = models.AutoField(primary_key=True, unique=True)
     user_id = models.ForeignKey(User)
     location_id = models.ForeignKey(Location)
     date = models.DateTimeField(default=timezone.now)
--- travel/views.py
+++ travel/views.py
@@ -6,18 +6,18 @@ from .serializers import UserSerializer, LocationSerializer, VisitSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = IsAuthenticated
+    # permission_classes = IsAuthenticated
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class LocationViewSet(viewsets.ModelViewSet):
-    permission_classes = IsAuthenticated
+    # permission_classes = IsAuthenticated
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
 
 
 class VisitViewSet(viewsets.ModelViewSet):
-    permission_classes = IsAuthenticated
+    # permission_classes = IsAuthenticated
     queryset = Visit.objects.all()
     serializer_class = VisitSerializer
